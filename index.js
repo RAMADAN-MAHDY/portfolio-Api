7d8faac94c2b9f1191af7198dafb5425609f1979
RAMADAN-MAHDY <ramadanmahdy45@gmail.com>
2025-03-09 01:40:41 +0200

    Setup Iron Session with 1-month expiry and production-ready security settings

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import MongoStore from "connect-mongo";
 import express from 'express';
 import cors from 'cors';
 import connectDB from './db.js';
@@ -6,18 +5,17 @@ import ContactRouter from './router/ContactRouter.js';
 import ContactRouterAdmin from './router/ContactRouterAdmin.js';
 import getMessage from './router/getMessage.js'; 
 import getConversations from './router/getConversations.js';
-import session from 'express-session';
+import cookieParser from "cookie-parser";
 import Dotenv from 'dotenv';
 import chatRouter from './router/chat.js';
 import getMessageAdmin from './router/getMessageToAdmin.js';
 import subscription from './router/save-subscription.js';
 import unsubscription from './router/unsubscribe.js';
 import sendNotificationToAll from './webpush/sendNotificationToAll.js';
+import { getIronSession } from "iron-session";
 Dotenv.config();
 
-
 const app = express();
-
 const port = 4000;
 
 //Purpose: To allow the frontend to access the backend
@@ -26,25 +24,30 @@ const corsOptions = {
     optionsSuccessStatus: 200,
     credentials: true
   }
-  
-app.use(cors(corsOptions));
+  //corsOptions
+  app.use(cors(corsOptions));
 connectDB();
-const uri = process.env.DB_KEY;
 
 app.use(express.json());
+app.use(cookieParser());
 
 
-app.use(session({
-    secret: process.env.session_secret_key,
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({
-        mongoUrl: uri,
-        ttl: 14 * 24 * 60 * 60 // 14 يوم
-    }),
-    cookie: { secure: false } // في الإنتاج خليها `true` لو عندك HTTPS
-}));
-
+// إعداد الـ session middleware
+app.use(async (req, res, next) => {
+    req.session = await getIronSession(req, res, {
+        cookieName: "session",
+        password: process.env.session_secret_key,
+        cookieOptions: {
+            secure: process.env.NODE_ENV === "production",  // خليها false أثناء التطوير
+            httpOnly: true,
+            sameSite: "strict", // تأكد إنها "lax" وليس "strict"
+            maxAge: 60 * 60 * 24 * 30,
+        },
+    });
+    next();
+});
+ 
+  // "production"
 
 // Purpose: To handle the routes for the contact form
 app.use('/contact', ContactRouter);
@@ -59,8 +62,14 @@ app.use('/sendNotificationToAll', sendNotificationToAll);
 
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World! 1')
+app.get('/', async (req, res) => {
+    if (!req.session.views) {
+        req.session.views = 1;
+      } else {
+        req.session.views++;
+      }
+      await req.session.save();
+      res.send(`عدد زياراتك: ${req.session.views}`);
 })
 
 app.listen(port, () => {
